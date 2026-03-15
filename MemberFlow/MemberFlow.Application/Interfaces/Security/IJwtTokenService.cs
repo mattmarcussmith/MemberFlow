6d9ba1252156c84d96579b28f2978702c17dbbe7
Matthew Smith <matt.marcus.smith@gmail.com>
2026-03-15 17:11:10 -0400

    wip: add JWT service

---
+++ MemberFlow/MemberFlow.Application/Interfaces/Security/IJwtTokenService.cs
@@ -0,0 +1,9 @@
+﻿using MemberFlow.Domain.Entities;
+
+namespace MemberFlow.Application.Interfaces.Security
+{
+    public interface IJwtTokenService
+    { 
+           string GenerateToken(User user);
+    }
+}
